b09ea88f723d90ba03883877f322cbfc6bebe286
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-03-24 20:26:39 +0530

    database setup using docker

---
+++ convex/schema.js
@@ -0,0 +1,15 @@
+
+
+
+import { defineSchema } from "convex/server";
+import { defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    users:defineTable({
+        name:v.string(),
+        email:v.string(),
+        credits:v.number(),
+        subscriptionId:v.string(),
+    })
+})
