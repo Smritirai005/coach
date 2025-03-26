107f940ef46a62f875cdf0342b6cda80fc342d78
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-03-26 17:55:59 +0530

    added profile features

--- app/(main)/dashboard/_components/FeatureAssistant.jsx
+++ app/(main)/dashboard/_components/FeatureAssistant.jsx
@@ -3,14 +3,20 @@
 
 import React from 'react'
 import { useUser } from '@stackframe/stack'
+import { Button } from '@/components/ui/button';
 
 function FeatureAssistant() {
     const user=useUser();
   return (
     <div>
+      <div className='flex justify-between items-center'>
+        <div>
         <h2 className='font-medium text-gray-500'>My Workspace</h2>
-        <h2>Welcome back,{user?.displayName}</h2>
-      
+        <h2 className='text-3xl font-bold'>Welcome back,{user?.displayName}</h2>
+        </div>
+        <Button>Profile</Button>
+      </div>
+        
     </div>
   )
 }
