b3104c74147fe1f5900c7c196b0c8152277e1fbf
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-04-04 01:11:43 +0530

    added schema for discussion room

--- app/(main)/dashboard/_components/UserInputBox.jsx
+++ app/(main)/dashboard/_components/UserInputBox.jsx
@@ -31,7 +31,7 @@ function UserInputBox({children,ExpertList}) {
             <Textarea className="mt-2" placeholder="Enter your topic here ...."
             onChange={(e) => setTopic(e.target.value)}/>
 
-            <h2 className='text-black mt-5'>Enter a topic to naster your skills in {ExpertList.name}</h2>
+            <h2 className='text-black mt-5'>Select  your coaching expert {ExpertList.name}</h2>
             <div className='grid grid-cols-3 md:grid-cols-5 gap-6 mt-3'>
               {CoachingExpert.map((expert, index) => (
                 <div key={index} onClick={()=> setSelectedExpert(expert.name)}>
