ec9d1179f6a7d3a4561d3ca44e124e0efad911d3
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-04-05 01:51:38 +0530

    added discussion room ui

--- app/(main)/discussion-room/[roomid]/page.jsx
+++ app/(main)/discussion-room/[roomid]/page.jsx
@@ -6,6 +6,9 @@ import { useState, useEffect } from 'react'
 import { useQuery } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 import { ExpertList } from '@/services/Options'
+import Image from 'next/image'
+import { CoachingExpert } from '@/services/Options'
+
 function DiscussionRoom() {
     const {roomid} = useParams();
     const [expert, setExpert] = useState();
@@ -20,7 +23,27 @@ function DiscussionRoom() {
 
   return (
     <div>
-      <h2>Discussion room</h2>
+      <h2 className='text-lg font-bold'>{DiscussionRoomData?.coachingOption}</h2>
+      <div className='mt-5 grid grid-cols-1  lg:grid-cols-4 gap-10'>
+        <div className='lg:col-span-3 h-[60vh] bg-secondary border rounded-4xl flex flex-col justify-center items-center animate-pulse'>
+          <Image
+          src={expert?.avatar}
+          alt='Avatar'
+          width={100}
+          height={100}
+          className=" h-[80px] w-[80px] object-cover rounded-full"
+        />
+        <h2 className='text-gray-500'>{expert?.name}</h2>
+      
+      
+        </div>
+        <div>
+
+
+        </div>
+
+
+      </div>
     </div>
   )
 }
