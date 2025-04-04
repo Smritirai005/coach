128510e888ea6e2bde2a7e081226073e761d101b
_smriti_rai_ <140977626+Smritirai005@users.noreply.github.com>
2025-04-05 02:26:07 +0530

    done with the discussion room

--- app/(main)/discussion-room/[roomid]/page.jsx
+++ app/(main)/discussion-room/[roomid]/page.jsx
@@ -8,6 +8,9 @@ import { api } from '@/convex/_generated/api'
 import { ExpertList } from '@/services/Options'
 import Image from 'next/image'
 import { CoachingExpert } from '@/services/Options'
+import { UserButton } from '@stackframe/stack'
+import { Button } from '@/components/ui/button'
+
 
 function DiscussionRoom() {
     const {roomid} = useParams();
@@ -16,35 +19,48 @@ function DiscussionRoom() {
     console.log(DiscussionRoomData);
     useEffect(() => {
       if(DiscussionRoomData){
-        const Expert=ExpertList.find((item) => item.name === DiscussionRoomData.expertName)
+        const Expert=CoachingExpert.find((item) => item.name === DiscussionRoomData?.expertName)
         setExpert(Expert);
       }
     },[DiscussionRoomData])
 
   return (
-    <div>
+    <div className='-mt-12'>
       <h2 className='text-lg font-bold'>{DiscussionRoomData?.coachingOption}</h2>
-      <div className='mt-5 grid grid-cols-1  lg:grid-cols-4 gap-10'>
-        <div className='lg:col-span-3 h-[60vh] bg-secondary border rounded-4xl flex flex-col justify-center items-center animate-pulse'>
+      <div className='mt-5 grid grid-cols-1 lg:grid-cols-3  gap-10'>
+        <div className='lg:col-span-2 h-[60vh]'>
+        <div className='lg:col-span-2 h-[60vh] bg-secondary border rounded-4xl flex flex-col justify-center items-center relative'>
           <Image
           src={expert?.avatar}
           alt='Avatar'
           width={100}
           height={100}
-          className=" h-[80px] w-[80px] object-cover rounded-full"
+          className=" h-[80px] w-[80px] object-cover rounded-full animate-pulse"
         />
         <h2 className='text-gray-500'>{expert?.name}</h2>
-      
-      
+        <div className='p-5 bg-gray-200 px-10 rounded-lg absolute bottom-10 right-10'>
+          <UserButton></UserButton>
         </div>
-        <div>
+        </div>
+        <div className='mt-5 flex item-center justify-center'>
+          <Button>Connect</Button>
+        </div>
+        </div>
 
+        <div>
+          <div className=' h-[60vh] bg-secondary border rounded-4xl flex flex-col justify-center items-center relative'>
+          <h2>Chat Section</h2>
+
+
+           </div>
+           <h2 className='mt-4 text-gray-400 text-sm'>At the end of your conversation e will automatically generate feedback/notes</h2>
 
         </div>
 
 
       </div>
     </div>
+
   )
 }
 
